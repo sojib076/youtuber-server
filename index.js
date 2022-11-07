70a7f7ae9c29128b9091d6700038e7b571d928f4
sojib076 <sojibds332@gmail.com>
2022-11-07 22:34:16 +0600

    mongodbadeed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
  require('dotenv').config();
 app.use(cors());
@@ -10,10 +11,28 @@ app.use(express.json());
 /// mongodb connection
 
 
+const uri = `mongodb+srv://${process.env.Mongodb_name}:${process.env.Mongodb_Password}@cluster0.pucpolg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-
-
+// mongodb api
+const run = () => {
+    const service = client.db("youtuber").collection("services");
+    try {
+        app.get('/service', async(req, res) => {
+            const query = {};
+            const result = await service.find(query).toArray();
+             res.send(result);
+             console.log(result);
+    
+        })
+    } catch (error) {
+        console.log(error);
+    }
+        
+    
+}
+run();
 
 
 //listen to port
