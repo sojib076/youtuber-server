3c684412e27ef428ee8afcf2d711f51ac831e54c
sojib076 <sojibds332@gmail.com>
2022-11-08 12:25:06 +0600

    reviews

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
  require('dotenv').config();
 app.use(cors());
@@ -16,29 +16,46 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 // mongodb api
-const run = () => {
+const run = async() => {
     const service = client.db("youtuber").collection("services");
+    const reviews = client.db("youtuber").collection("reviews");
     try {
         app.get('/service', async(req, res) => {
             const query = {};
             const cursor = service.find(query).limit(3);
             const result = await cursor.toArray();
              res.send(result);
-             console.log(result);
-    
+             
         })
         app.get('/services', async(req, res) => {
             const query = {};
             const result = await service.find(query).toArray();
              res.send(result);
-             console.log(result);
     
         })
-        app.post('/addService', async(req, res) => { 
-            const result = await service.insertOne(req.body);
-            res.send(result);
-            console.log(result);
+        
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)}
+            const result = await service.findOne(query);
+             res.send(result);
         })
+        app.post('/reviews', async(req, res) => {
+            const query = req.body;
+            // fetch data from serviceid);
+            console.log(req.headers.serviceid);
+            const result = await reviews.insertOne(query);
+             res.send(result);
+        })
+        app.get('/reviews', async(req, res) => { 
+            const query = req.headers.serviceid
+            
+
+        
+            const result = await reviews.find({"serviceid":query}).toArray();
+                res.send(result);
+        })
+
     } catch (error) {
         console.log(error);
     }
